c27700642275c7dbccd3dc3e857a64c22e781a7c
Yiğit GÜLEÇ <githubda-kullandigin-email@example.com>
2026-04-20 22:56:44 +0300

    feat: complete inheritance projects and fix package structure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,24 @@
+package com.workintech;
+
+import com.workintech.cylinder.Circle;
+import com.workintech.cylinder.Cylinder;
+import com.workintech.pool.Cuboid;
+import com.workintech.pool.Rectangle;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Circle circle = new Circle(3.75);
+        System.out.println("circle.radius = " + circle.getRadius());
+        System.out.println("circle.area = " + circle.getArea());
+
+        Cylinder cylinder = new Cylinder(5.55, 7.25);
+        System.out.println("cylinder.volume = " + cylinder.getVolume());
+
+        Rectangle rectangle = new Rectangle(5, 10);
+        System.out.println("rectangle.area = " + rectangle.getArea());
+
+        Cuboid cuboid = new Cuboid(5, 10, 5);
+        System.out.println("cuboid.volume = " + cuboid.getVolume());
     }
 }
